c91a8e0231864e362fea1b5328764f8c2d2e9c08
Aroj1986 <116952156+Aroj1986@users.noreply.github.com>
2022-12-16 10:00:57 +0100

    Update Footer.js

--- hacker-news/src/Components/Footer.js
+++ hacker-news/src/Components/Footer.js
@@ -40,43 +40,6 @@ function Footer({setQueryProp}) {
                 >
                 </input>
                 <button onClick={addSearchKeyword}>Search</button>
-            </div>
-function Footer({ setQuery }) {
-  const [searchKeyword, setSearchKeyword] = useState("");
-
-  // const addSearchKeyword = ()=>{
-  //     if(!inputValue){
-  //         return "Type some keywords"
-  //     } else {
-  //         setSearchKeyword([...searchKeyword, inputValue])
-  //         setInputValue("")
-  //     }
-  // }
-
-  return (
-    <div className="container-footer">
-      <div className="row">
-        <div className="col-12">
-          <hr className="horizontal-break" />
-          <nav className="termsAndConditions">
-            <p>Guidelines </p>
-            <p>FAQ </p>
-            <p>Lists </p>
-            <p>API </p>
-            <p>Security </p>
-            <p>Legal </p>
-            <p>Apply to YC </p>
-            <p>Contact</p>
-          </nav>
-          <div className="searchField">
-            <input
-              type={"search"}
-              value={searchKeyword}
-              placeholder="Type new keywords here"
-              onChange={(e) => setSearchKeyword(e.target.value)}
-            />
-
-            <button onClick={()=> {setQuery(searchKeyword)}}>search</button>
           </div>
         </div>
       </div>
