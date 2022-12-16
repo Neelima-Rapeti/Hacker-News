c6941aa74e0599a53faae67ab61c014249fe4e60
Aroj1986 <116952156+Aroj1986@users.noreply.github.com>
2022-12-16 09:57:18 +0100

    Update App.js

--- hacker-news/src/App.js
+++ hacker-news/src/App.js
@@ -54,31 +54,3 @@ import Footer from './Components/Footer';
 import axios from "axios";
 
 export default function App() {
-
-
-const [searchKeyword, setSearchKeyword] = useState('');
-const [posts, setPosts] = useState([]);
-const [query,setQuery]= useState("react");
-console.log();
-
-     useEffect(() => {
-      axios.get(`http://hn.algolia.com/api/v1/search?query=${query}`)
-      .then((response) => {
-      setPosts(response.data.hits);
-     });
-      }, [query]);
-  return (
-  <div>
-    <Header />
-    {posts.map((post) => {
-                return(
-                  <li> {post.title}</li>
-
-                ) })}
-  <Footer setQuery={setQuery} />
-    </div>
-  
-  );
-}
-
-
